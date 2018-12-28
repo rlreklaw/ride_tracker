5395fe0fecfe72685d7503bf527bcb3d5f4ff96d
rwalker <robpub@robonco.com>
2018-12-28 16:28:04 -0600

    Return newly created Ride object (w/ id)

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.pluralsight.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -7,7 +9,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -29,10 +34,33 @@ public class RideRepositoryImpl implements RideRepository {
 	@Override
 	public Ride createRide(Ride ride) {
 
-		jdbcTemplate.update("insert into ride (name, duration) values (?, ?)", 
-				ride.getName(), ride.getDuration());
+//		jdbcTemplate.update("insert into ride (name, duration) values (?, ?)", 
+//				ride.getName(), ride.getDuration());
+		
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				
+				PreparedStatement ps = con.prepareStatement("insert into ride (name, duration) values (?, ?)",
+						new String[] {"id"});
+				ps.setString(1, ride.getName());
+				ps.setInt(2, ride.getDuration());
+				
+				return ps;
+			}
+		}, keyHolder);
+		
+		Number id = keyHolder.getKey();
 
-		return null;
+		return getRide(id.intValue());
+	}
+
+	public Ride getRide(Integer id) {
+		Ride ride = jdbcTemplate.queryForObject("select * from ride where id = ?", new RideRowMapper(), id);
+		
+		return ride;
 	}
 
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -18,8 +18,8 @@ public class RestControllerTest {
 		RestTemplate restTemplate = new RestTemplate();
 		
 		Ride ride = new Ride();
-		ride.setName("Willow Trail Ride");
-		ride.setDuration(30);
+		ride.setName("Sagebrush Trail");
+		ride.setDuration(42);
 		
 		ride = restTemplate.postForObject("http://localhost:8080/ride_tracker/ride", ride, Ride.class);
 		
