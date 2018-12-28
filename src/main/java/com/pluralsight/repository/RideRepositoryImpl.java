0e496eee7c139db21f00f232138bc879d17b32e6
rwalker <robpub@robonco.com>
2018-12-28 11:18:21 -0600

    Add createRide feature
    Complete end-to-end implementation, controller to repository.  Also
    added a test method.

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -14,15 +14,24 @@ public class RideRepositoryImpl implements RideRepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	@Override
 	public List<Ride> getRides() {
 		Ride ride = new Ride();
 		ride.setName("Corner Canyon");
 		ride.setDuration(120);
-		List <Ride> rides = new ArrayList<>();
+		List<Ride> rides = new ArrayList<>();
 		rides.add(ride);
 		return rides;
 	}
-	
+
+	@Override
+	public Ride createRide(Ride ride) {
+
+		jdbcTemplate.update("insert into ride (name, duration) values (?, ?)", 
+				ride.getName(), ride.getDuration());
+
+		return null;
+	}
+
 }
